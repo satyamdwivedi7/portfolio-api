04e2d67fc3717a22b69651507264f10d859e3c98
Satyam Dwivedi <122256218+satyam7579@users.noreply.github.com>
2024-04-20 21:20:06 +0530

    update function added

--- controller/skills.controller.js
+++ controller/skills.controller.js
@@ -1,27 +1,33 @@
-const Skill = require("../model/skills")
+const Skill = require("../model/skills");
 
-
-module.exports.createSkill = async (req, res) =>{
-    const {title, skillSet} = req.body;
-    console.log(req.body)
-    try{
-        const skill = new Skill({
-            title, 
-            skillSet
-        })
-        await skill.save()
-        res.status(201).send(skill)
-    }catch(err){
-        console.log(err)
-        res.status(400).send(err)
+module.exports.createSkill = async (req, res) => {
+  const { title, skillSet } = req.body;
+  try {
+    let check = await Skill.findOne({ title });
+    if (check) {
+      let skill = await Skill.findOneAndUpdate({ title });
+      skill.skillSet.push(...skillSet);
+      await Skill.findOneAndUpdate(skill._id, skill);
+      res.status(201).send(skill);
+    } else {
+      const skill = new Skill({
+        title,
+        skillSet,
+      });
+      await skill.save();
+      res.status(201).send(skill);
     }
-}
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err);
+  }
+};
 
-module.exports.getSkill = async(req, res) =>{
-    try {
-      const skills = await Skill.find();
-      res.status(200).send(skills);
-    } catch (error) {
-      res.status(400).send(error);
-    }
-}
+module.exports.getSkill = async (req, res) => {
+  try {
+    const skills = await Skill.find();
+    res.status(200).send(skills);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
